11e5d94cb31b89486a30df4d2c4a17f32748cbf2
Christophe anfry <frybcn@gmail.com>
2023-11-29 21:36:04 +0100

    Create API Route and sendEmail function

--- src/pages/api/sendEmail.json.ts
+++ src/pages/api/sendEmail.json.ts
@@ -3,13 +3,25 @@ import { Resend } from 'resend';
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
-export const GET: APIRoute = async ({ params, request }) => {
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.formData();
+  const name = data.get('name');
+  const email = data.get('email');
+  const message = data.get('message');
+  if (!name || !email || !message) {
+    return new Response(
+      JSON.stringify({
+        message: 'Missing required fields',
+      }),
+      { status: 400 }
+    );
+  }
   const send = await resend.emails.send({
     from: 'info@anfrydev.com',
     to: 'frybcn@gmail.com',
-    subject: 'hello world',
-    html: '<p>Hi</p>',
-    text: 'it works!',
+    subject: 'New contact from AnfryDev',
+    html: `<p>Name: ${name}</p><br /><p>Name: ${email}</p><br /><p>Name: ${message}</p>`,
+    text: `Name: ${name} Email: ${email} message: ${message}`,
   });
 
   if (send.data) {
